e1b65691cb5abf9f5218455d7c2fa56aa610dc49
Narges-Akhlaghi <a.nrgsai@gmail.com>
2024-07-11 15:24:17 +0330

    deleted

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8089/api'; // Replace with your backend URL
-
-const axiosInstance = axios.create({
-  baseURL: API_URL,
-});
-
-export default axiosInstance;
