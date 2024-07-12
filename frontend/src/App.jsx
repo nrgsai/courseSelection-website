0fbf74ebd012eb6f4d31c199bec50a5cb10090f5
Narges-Akhlaghi <a.nrgsai@gmail.com>
2024-07-12 17:40:07 +0330

    i change it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,30 @@
 import "./App.css";
-import "./styles/Admin.css"
-import "./styles/Auth.css"
-import "./styles/Student.css"
+import "./styles/Admin.css";
+import "./styles/Auth.css";
+import "./styles/Student.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import AuthService from "./services/AuthService";
-import  Login from "./components/Auth/Login";
-import  Register  from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 import StudentDashboard from "./components/Student/StudentDashboard";
 import AdminDashboard from "./components/Admin/AdminDashboard";
 
 const App = () => {
-  const currentUser = AuthService?.getCurrentUser();
+  const currentUser = AuthService.getCurrentUser();
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route  path="/" element={<Login/>} />
-          <Route  path="/login" element={<Login/>} />
-          <Route  path="/register" element={<Register/>} />
-          {currentUser && currentUser.role === "admin" && (
-            <Route path="/admin" component={AdminDashboard} />
-          )}
-          {currentUser && currentUser.role === "student" && (
-            <Route path="/student" component={StudentDashboard} />
-          )}
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {currentUser && currentUser.role === "admin" && (
+          <Route path="/admin" element={<AdminDashboard />} />
+        )}
+        {currentUser && currentUser.role === "student" && (
+          <Route path="/student" element={<StudentDashboard />} />
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
