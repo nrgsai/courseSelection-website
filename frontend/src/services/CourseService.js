53c0ad1c3cabb79417b494a12d33a048d03d4333
dev <mohammad.ali.asheghi@gmail.com>
2024-07-13 11:04:42 +0330

    roll back link

--- frontend/src/services/CourseService.js
+++ frontend/src/services/CourseService.js
@@ -1,6 +1,6 @@
 import axiosInstance from './api';
 
-const API_URL = 'https://738d-86-55-248-175.ngrok-free.app'; // Assuming this is the base URL for your backend
+const API_URL = 'http://localhost:8089'; // Assuming this is the base URL for your backend
 
 const getCourses = () => {
     return axiosInstance.get(`${API_URL}/course/list`).then((response) => response.data);
